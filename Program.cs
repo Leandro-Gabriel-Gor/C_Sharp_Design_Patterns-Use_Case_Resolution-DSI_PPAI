042d3e94597566dec75310969d997cefd5024b81
leandrogor <leandrogabrielgor@gmail.com>
2022-06-23 14:39:45 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,9 +8,6 @@ namespace PPAI_DSI
 {
     static class Program
     {
-        /// <summary>
-        /// Punto de entrada principal para la aplicación.
-        /// </summary>
         [STAThread]
         static void Main()
         {
